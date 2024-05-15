047f0ba74affff7f37d691dfaf828bb9b1be0d04
GiaTrong2003 <tronghghe172557@fpt.edu.vn>
2024-05-15 11:56:29 +0700

    feat: Trang header và footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import './App.css';
 import AllRoute from './components/AllRoutes/AllRoute';
+import './assets/css/animate.min.css'
+import './assets/css/bootstrap.min.css'
+import './assets/css/font-awesome.min.css'
+import './assets/css/magnific-popup.min.css'
+import './assets/css/nice-select.css'
+import './assets/css/Pe-icon-7-stroke.css'
+import './assets/css/swiper-bundle.min.css'
 
 function App() {
   return (
