b450e680b68ae2050043af8e686abc84c9b9a30b
baovlhe172612 <baovlhe172612@fpt.edu.vn>
2024-05-15 13:11:01 +0700

    Update LayoutDefault.js

--- src/Layout/LayoutDefault/LayoutDefault.js
+++ src/Layout/LayoutDefault/LayoutDefault.js
@@ -1,5 +1,6 @@
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import Cart from "../../pages/Client/Cart/Cart";
 import { Outlet } from "react-router-dom";
 
 function LayoutDefault() {
@@ -12,6 +13,8 @@ function LayoutDefault() {
       </div>
 
       <Footer />
+
+      <Cart  />
     </>
   );
 }
