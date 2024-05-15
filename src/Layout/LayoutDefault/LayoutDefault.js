8007cfed967baff8729c918a69bdae9b54843084
GiaTrong2003 <tronghghe172557@fpt.edu.vn>
2024-05-15 13:25:07 +0700

    feat: Cart

--- src/Layout/LayoutDefault/LayoutDefault.js
+++ src/Layout/LayoutDefault/LayoutDefault.js
@@ -1,3 +1,4 @@
+import Cart from "../../pages/Client/Cart/Cart";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import { Outlet } from "react-router-dom";
@@ -7,9 +8,11 @@ function LayoutDefault() {
     <>
       <Header />
 
-      <div>
+      {/* <div>
         <Outlet />
-      </div>
+      </div> */}
+
+      <Cart />
 
       <Footer />
     </>
