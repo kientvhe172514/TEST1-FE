63bffa67b8f61e3464a567b4850d2f9893fdb5bf
GiaTrong2003 <111938146+GiaTrong2003@users.noreply.github.com>
2024-05-16 15:50:32 +0700

    feat: Demo chart và sửa lại router

--- src/pages/Admin/Dashboard/Dashboard.js
+++ src/pages/Admin/Dashboard/Dashboard.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import {
+  Chart as ChartJS,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
 
 function Dashboard() {
+  const data = {
+    labels: ["Mon", "Tue", "Wed"],
+    datasets: [
+      {
+        label: "369",
+        data: [3, 6, 9],
+        backgroundColor: "aqua",
+        borderColor: "black",
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {};
+
   return (
-    <div>Dashboard</div>
-  )
+    <>
+      <Bar data={data} options={options} />
+    </>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
